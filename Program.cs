d91f46e8c525972496747f5fa9b2fef3b4f978c6
Zeeshan Sarwar <zeeshansarwar2968@gmail.com>
2022-05-08 17:10:31 +0530

    First Commit to initialise the project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("\t\t\tLogical Programs in C#");
+            Console.ReadKey();
+        }
+    }
+}
