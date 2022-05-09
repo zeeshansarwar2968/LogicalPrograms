198f7360d055044418c7de4b3c9b3b7707a5190e
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-09 15:45:31 +0530

    Added logic for printing fibonacci series

---
+++ FibonacciSeries.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public void calculationLogic()
+        {
+            //initialsing variables to use in the program
+            int n1 = 0, n2 = 1, n3, i, number;
+
+            //taking in input from the user
+            Console.Write("Enter the number of elements: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            //printing 0 and 1 which are the initial values of the series
+            Console.Write(n1 + " " + n2 + " ");
+
+            //Main Logic loop, loop starts from 2 because 0 and 1 are already printed
+            for (i = 2; i < number; ++i)     
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ namespace LogicalPrograms
         {
             Console.WriteLine("");
             Console.WriteLine("\t\t\tLogical Programs in C#");
+            Console.WriteLine("");
+
+            FibonacciSeries series = new FibonacciSeries();
+            series.calculationLogic();
+
             Console.ReadKey();
         }
     }
