3bd8da57f03bac6b28b987a5ebcb3a90370623fc
zeeshansarwar2968 <zeeshansarwar2968@gmail.com>
2022-05-10 11:07:09 +0530

    [Zeeshan] : Added Program for Calculating Perfect Number

---
+++ PerfectNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PerfectNumber
+    {
+        public void PerfectNumberLogic()
+        {
+            //initialsing variables to use in the program
+            int sum=0, number;
+            string divisors = "";
+            //taking in input from the user
+            Console.Write("Enter the number to check for perfect number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            //initiating a for loop for times as inut by the user encomposing the logic
+            for (int i = 1; i < number; i++)
+            {   
+                if (number % i == 0)    //if he number is divisible by i, then add to the sum and append that to the divisors string
+                {
+                    sum += i;
+                    divisors += i.ToString() + "+";
+                }
+            }
+            //To remove the last "+" appended in the string divisors
+            divisors = divisors.Remove(divisors.Length - 1, 1);
+
+            Console.WriteLine(divisors + " = " + sum);
+
+            //To check if the number is perfect and log the output
+            if (sum == number)
+            {
+                Console.WriteLine("Thus , " + number + " Is A Perfect Number");
+            }
+            else
+                Console.WriteLine("Thus , " + number + " Is Not A Perfect Number");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,11 +9,45 @@ namespace LogicalPrograms
             Console.WriteLine("");
             Console.WriteLine("\t\t\tLogical Programs in C#");
             Console.WriteLine("");
+            Console.WriteLine("1. Fibonacci Series");
+            Console.WriteLine("");
+            Console.WriteLine("2. Perfect Number");
+            Console.WriteLine("");
+            Console.WriteLine("\t\t\t Please press 3 to exit");
+            Console.WriteLine("");
+            //FibonacciSeries series = new FibonacciSeries();
+            //series.calculationLogic();
+            
+            int flag = 0;
+            while (flag == 0)
+            {
+                Console.Write("\nPlease Enter Your Option to choose the program to execute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.calculationLogic();
+                        break;
+                    case 2:
+                        PerfectNumber perfectNumber = new PerfectNumber();
+                        perfectNumber.PerfectNumberLogic();
+                        break;
+                    case 3:
+                        flag = 1;
+                        break;
+                    default:
+                        Console.WriteLine("Please Enter from the given choice");
+                        break;
+                  
 
-            FibonacciSeries series = new FibonacciSeries();
-            series.calculationLogic();
+                }
 
-            Console.ReadKey();
+            }
+            
+
+            
+            
         }
     }
 }
